bd04941170e3c3dbad85bb83555897f4149ea0cc
Ajeet Kumar Soni <94032343+Ajeetsoni2002@users.noreply.github.com>
2025-04-27 16:59:58 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ import SignUpPage from "./pages/SignUpPage";
 import LoginPage from "./pages/LoginPage";
 import SettingsPage from "./pages/SettingsPage";
 import ProfilePage from "./pages/ProfilePage";
-import { useAuthStore } from "./store/UseAuthStore";
+import { useAuthStore } from "./store/useAuthStore";
 import { useThemeStore } from "./store/useThemeStore";
 import { useEffect } from "react";
 import {Loader} from "lucide-react";
